0e82d4db7293b986ef6a43d3029f94ebb6a12300
Adailsonacj <adailsonacj@live.com>
2018-10-20 02:22:37 -0300

    Tanques

--- app/src/main/java/com/sharknado/gestfish_hfc2018/Home.java
+++ app/src/main/java/com/sharknado/gestfish_hfc2018/Home.java
@@ -1,7 +1,9 @@
 package com.sharknado.gestfish_hfc2018;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void onClick(View view) {
+        Intent vrintention = new Intent(this, Tanques.class);
+        startActivity(vrintention);
+        finish();
     }
 }
